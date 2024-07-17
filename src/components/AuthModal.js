76ae71029c2984b6e7e77e066adbf3ba5e08cec0
Peter michoma <pmichoma@kabarak.ac.ke>
2024-07-17 12:42:49 -0400

    modal form

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -3,11 +3,13 @@ const AuthModal = ({setShowModal}) =>{
     const handleClick =() =>{
         setShowModal(false)
     }
-
+    const isSignUp =false;
     return(
         <div className="auth-modal"> 
-         <div onClick={handleClick}>X</div>
+         <div className="close-icon" onClick={handleClick}>X</div>
          AUTH MODAL
+         <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
+         <p>By Clicking Login you accept to our terms and conditions</p>
         </div>
     )
 }
