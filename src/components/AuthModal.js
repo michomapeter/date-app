bebfd5299c0ef0f1f8926947f1b41668a2bddc6c
Peter michoma <pmichoma@kabarak.ac.ke>
2024-07-18 16:46:18 -0400

    addition of the login credentials to the modal

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,7 +1,17 @@
-const AuthModal = ({setShowModal}) =>{
+import {useState} from 'react'
 
+const AuthModal = ({setShowModal}) =>{
+   const[email ,setEmail] = useState(null)
+   const [password ,setPassword] = useState(null)
+   const [confirmPassword ,setconfirmPassword] =useState(null)
+   const [error ,setError] = useState(null)
+
+   console.log(email ,password ,confirmPassword)
     const handleClick =() =>{
         setShowModal(false)
+    }
+    const handleSubmit =(e) =>{
+        e.preventDefault()
     }
     const isSignUp =false;
     return(
@@ -10,6 +20,34 @@ const AuthModal = ({setShowModal}) =>{
          AUTH MODAL
          <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
          <p>By Clicking Login you accept to our terms and conditions</p>
+
+         <form onSubmit={handleSubmit}>
+          <input
+          type ="email"
+          id ="email"
+          name="email"
+          placeholder="email"
+          required= {true}
+          onChange={(e)=>setEmail(e.target.value)}
+          />
+           <input
+          type ="password"
+          id ="password"
+          name="password"
+          placeholder="password"
+          required= {true}
+          onChange={(e)=>setPassword(e.target.value)}
+          />
+           <input
+          type ="password-check"
+          id ="password-check"
+          name="password-check"
+          placeholder="password-check"
+          required= {true}
+          onChange={(e)=>setconfirmPassword(e.target.value)}
+          />
+
+         </form>
         </div>
     )
 }
