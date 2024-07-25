4f31c5414e731178e257c474feec1c5e99b3f57c
Peter michoma <pmichoma@kabarak.ac.ke>
2024-07-24 23:08:10 -0400

    message

---
+++ client/src/components/ChatDisplay.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Chat  from '../components/Chat'
+import ChatInput from '../components/ChatInput'
+
+function ChatDisplay() {
+  return (
+  <>
+  <Chat/>
+  <ChatInput/>
+  </>
+  )
+}
+
+export default ChatDisplay
